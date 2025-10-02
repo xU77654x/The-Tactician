cc7c151b01889f5884cf26f025ae0fda6dcab012
Spencer Child <spencerchild947@protonmail.com>
2025-10-01 18:55:17 -0700

    1.1.3 release

--- src/main/java/tactician/effects/cards/CuttingGaleEffect.java
+++ src/main/java/tactician/effects/cards/CuttingGaleEffect.java
@@ -3,6 +3,7 @@ package tactician.effects.cards;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
@@ -16,16 +17,18 @@ public class CuttingGaleEffect extends AbstractGameEffect {
 	private final String soundKey;
 	private final float volume;
 	private boolean fired;
+	private Color windColor;
 	private int count = 0;
 	private float timer = 0.0F;
 
 	public CuttingGaleEffect(float x, float y, String soundKey, float volume) {
 		this.x = x;
 		this.y = y;
-		this.color = new Color(0.8F, 1.0F, 0.85F, 0.9F);
+		this.windColor = new Color(0.8F, 1.0F, 0.85F, 0.9F);
+		this.color = new Color(0.5F, 1.0F, 0.67F, 1.0F);
 		this.soundKey = soundKey;
 		this.volume = volume;
-		this.duration = 1.5F;
+		// this.duration = 1.5F;
 	}
 
 	@Override
@@ -37,22 +40,23 @@ public class CuttingGaleEffect extends AbstractGameEffect {
 		this.timer -= Gdx.graphics.getDeltaTime();
 		if (this.timer < 0.0F) {
 			this.timer += 0.05F;
-			if (this.count == 0) { AbstractDungeon.effectsQueue.add(new BorderLongFlashEffect(this.color.cpy())); }
-			AbstractDungeon.effectsQueue.add(new WindyParticleEffect(this.color, false));
+			if (this.count == 0) { AbstractDungeon.effectsQueue.add(new BorderLongFlashEffect(this.windColor.cpy())); }
+			AbstractDungeon.effectsQueue.add(new WindyParticleEffect(this.windColor.cpy(), false));
 			this.count++;
 			if (this.count == 10) {
 				fired = false;
 				this.isDone = true;
 			}
 		}
+		if (this.timer < 0.045F) { this.color.a = Interpolation.fade.apply(0.0F, 1.0F, (1.0F - this.timer) * 10.0F); }
+		else { this.color.a = Interpolation.pow2Out.apply(0.0F, 1.0F, this.timer); }
 	}
 
 	@Override
 	public void render(SpriteBatch sb) {
-		sb.setColor(0.5F, 1.0F, 1.0F, 1.0F);
+		sb.setColor(this.color);
 		sb.draw(ImageMaster.INTENT_MAGIC_L, this.x - 256F, this.y - 256F, 128.0F * this.scale * 4, 128.0F * this.scale * 4, 0.0F,0.0F, 1F, 1F);
 	}
-	// TODO: This sprite needs to fade in and fade out.
 
 	@Override
 	public void dispose() {}
